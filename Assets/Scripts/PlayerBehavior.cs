debcad6061330c5085d830e971c80041b0d7c070
Haykys <Haykys@gmail.com>
2019-03-24 16:03:29 +0100

    Imported Sprites, Created Player Movement, Created first bouncing object

---
+++ Assets/Scripts/PlayerBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehavior : MonoBehaviour
+{
+    // config params
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 5f;
+    [SerializeField] float screenWidthInUnits = 6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        Vector2 playerPos = new Vector2(transform.position.x, transform.position.y);
+        playerPos.x = Mathf.Clamp(mousePosInUnits, minX, maxX);
+        transform.position = playerPos;
+    }
+}
